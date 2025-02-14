a5f5f1b0465cd0c9662907952351b324eb84bb70
Mannan Ahmad <mannan.a90@outlook.com>
2025-02-14 06:44:11 -0700

    Fixed syntax errors in CSC450_CT1_Mod1-2.cpp and successfully compiled

---
+++ eclipse-workspace/CSC450_CT1_Modd1/CSC450_CT1_Mod1-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// Allows usage of standard C++ library without std:: prefix
+using namespace std;
+//Main Function
+int main() {
+// This should be printed out
+    double myMoney = 1000.50;
+
+// Standard Output Statement
+    cout << "Please be sure to correct all syntax errors in this program." << endl;
+    cout << "I have corrected all errors for this program." << endl;
+    cout << "The total amount of money available is = " << myMoney << endl;
+    // Return statement to indicate successful execution
+    return 0;
+}
