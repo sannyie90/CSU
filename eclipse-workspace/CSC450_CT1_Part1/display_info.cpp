a4c0ae6101808e733a0a99285c2d03c02504abdd
Mannan Ahmad <mannan.a90@outlook.com>
2025-02-14 07:42:13 -0700

    Created a program to display personal information of a fictional person

---
+++ eclipse-workspace/CSC450_CT1_Part1/display_info.cpp
@@ -0,0 +1,20 @@
+// Program: Display Personal Information
+// Author: Mannan Ahmad
+// Date: 02/14/2025
+// Description: This program displays personal information of a fictional person.
+
+#include <iostream>  // Required for input-output operations
+using namespace std; // Allows usage of standard C++ library without std:: prefix
+
+// Main Function
+int main() {
+    // Display Personal Information
+    cout << "First Name: Zainab" << endl;
+    cout << "Last Name: Amin" << endl;
+    cout << "Street Address: 1414 U Street" << endl;
+    cout << "City: Washington DC" << endl;
+    cout << "Zip Code: 12345" << endl;
+
+    // Return statement to indicate successful execution
+    return 0;
+}
