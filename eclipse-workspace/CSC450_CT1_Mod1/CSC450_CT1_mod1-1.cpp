13f0eb8642dcb196526e0627325d3df44b5353de
Mannan Ahmad <mannan.a90@outlook.com>
2025-02-14 07:20:29 -0700

    Added CSC450_CT1_mod1-1.cpp file to the repository after changes.

---
+++ eclipse-workspace/CSC450_CT1_Mod1/CSC450_CT1_mod1-1.cpp
@@ -0,0 +1,17 @@
+/*
+   Simple Program with a few Errors for Correction
+   Please be sure to correct all outlined errors.
+*/
+// Include necessary header for input/output operations
+#include <iostream>
+// Allows usage of standard C++ library without std:: prefix
+using namespace std;
+
+// Main Function
+int main() {
+// Standard Output Statement
+    cout << "Welcome to this C++ Program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+// Return statement to indicate successful execution
+    return 0;
+}
